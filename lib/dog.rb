83a764d590567fce41c9a5b40106f172910f7003
Leslie Stevens <flylady@mail.utexas.edu>
2018-03-23 22:53:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Dog
+  def initialize(name, bark)
+    @name = name
+    @bark = bark
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def bark=(bark)
+    @bark = woof!
+  end
+  def bark
+    @bark
+  end
+end
